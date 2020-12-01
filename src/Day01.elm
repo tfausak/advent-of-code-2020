0f46581d2e54cdfc1ec5270a529816daf5fb053f
Taylor Fausak <taylor@fausak.me>
2020-12-01 14:14:24 +0000

    Improve formatting

--- src/Day01.elm
+++ src/Day01.elm
@@ -1,6 +1,9 @@
 module Day01 exposing (main)
 
-{-| --- Day 1: Report Repair ---
+{-| <https://adventofcode.com/2020/day/1>
+
+
+# Day 1: Report Repair
 
 After saving Christmas five years in a row, you've decided to take a vacation
 at a nice resort on a tropical island. Surely, Christmas will go on without
@@ -26,31 +29,38 @@ multiply those two numbers together.
 
 For example, suppose your expense report contained the following:
 
-1721
-979
-366
-299
-675
-1456
+    1721
 
-In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying
-them together produces 1721 \* 299 = 514579, so the correct answer is 514579.
+    979
+
+    366
+
+    299
+
+    675
+
+    1456
+
+In this list, the two entries that sum to `2020` are `1721` and `299`.
+Multiplying them together produces `1721 * 299 = 514579`, so the correct answer
+is `514579`.
 
 Of course, your expense report is much larger. Find the two entries that sum to
-2020; what do you get if you multiply them together?
+`2020`; what do you get if you multiply them together?
 
---- Part Two ---
+
+## Part Two
 
 The Elves in accounting are thankful for your help; one of them even offers you
 a starfish coin they had left over from a past vacation. They offer you a
 second one if you can find three numbers in your expense report that meet the
 same criteria.
 
-Using the above example again, the three entries that sum to 2020 are 979, 366,
-and 675. Multiplying them together produces the answer, 241861950.
+Using the above example again, the three entries that sum to `2020` are `979`,
+`366`, and `675`. Multiplying them together produces the answer, `241861950`.
 
 In your expense report, what is the product of the three entries that sum to
-2020?
+`2020`?
 
 -}
 
