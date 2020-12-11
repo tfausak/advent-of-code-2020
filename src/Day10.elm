b42f2fc0f60467aae9df208ebb0281f356fdb854
Taylor Fausak <taylor@fausak.me>
2020-12-11 02:45:06 +0000

    Clean up day 10 a little

--- src/Day10.elm
+++ src/Day10.elm
@@ -8,55 +8,57 @@ import List.Extra
 part1 : String -> String
 part1 string =
     string
+        |> handleInput
+        |> Dict.Extra.frequencies
+        |> (\x -> Maybe.map2 (*) (Dict.get 1 x) (Dict.get 3 x))
+        |> Maybe.map String.fromInt
+        |> Maybe.withDefault ":("
+
+
+handleInput : String -> List Int
+handleInput string =
+    string
         |> String.lines
         |> List.filterMap String.toInt
         |> List.sort
-        |> (::) 0
+        |> addBounds
         |> pairs
         |> List.map (\( lo, hi ) -> hi - lo)
-        |> Dict.Extra.frequencies
-        |> solve
-        |> Maybe.map String.fromInt
-        |> Maybe.withDefault ":("
+
+
+addBounds : List Int -> List Int
+addBounds xs =
+    case List.maximum xs of
+        Nothing ->
+            xs
+
+        Just x ->
+            0 :: xs ++ [ x + 3 ]
+
+
+pairs : List a -> List ( a, a )
+pairs list =
+    List.map2 Tuple.pair list (List.drop 1 list)
 
 
 part2 : String -> String
 part2 string =
     string
-        |> String.lines
-        |> List.filterMap String.toInt
-        |> List.sort
-        |> (::) 0
-        |> pairs
-        |> List.map (\( lo, hi ) -> hi - lo)
+        |> handleInput
         |> List.Extra.group
         |> List.filter (\( x, _ ) -> x == 1)
-        |> List.map (\( _, xs ) -> List.length xs + 1)
-        |> List.map (\x -> tribonacci (x + 1))
+        |> List.map (\( _, xs ) -> tribonacci (List.length xs + 1))
         |> List.product
         |> String.fromInt
 
 
 tribonacci : Int -> Int
 tribonacci n =
-    if n < 1 then
+    if n < 0 then
         0
 
-    else if n < 3 then
+    else if n < 2 then
         1
 
     else
         tribonacci (n - 1) + tribonacci (n - 2) + tribonacci (n - 3)
-
-
-solve : Dict Int Int -> Maybe Int
-solve differences =
-    Maybe.map2
-        (\ones threes -> ones * (threes + 1))
-        (Dict.get 1 differences)
-        (Dict.get 3 differences)
-
-
-pairs : List a -> List ( a, a )
-pairs list =
-    List.map2 Tuple.pair list (List.drop 1 list)
