e221cffe057600409dd37fc38aeb4b0c368eeb99
Taylor Fausak <taylor@fausak.me>
2020-12-19 08:58:15 -0500

    Format day 16

--- src/Day16.elm
+++ src/Day16.elm
@@ -1,6 +1,5 @@
 module Day16 exposing (part1, part2)
 
-import List.Extra
 import Maybe.Extra
 import Set exposing (Set)
 
@@ -27,55 +26,102 @@ part1 string =
 
 
 part2 : String -> String
-part2 string = string
-    |> parseNotes
-    |> Maybe.map (\ notes ->
-        let
-            validTickets = notes.nearby
-                |> List.filter (isValidTicket notes.rules)
-            ruleCandidates = notes.rules
-                |> List.map (\ rule -> validTickets
-                    |> List.map (\ ticket -> ticket
-                        |> List.indexedMap Tuple.pair
-                        |> List.filterMap (\ ( index, value ) -> if inRule rule value then Just index else Nothing)
-                        |> Set.fromList)
-                    |> intersections
-                    |> Set.toList
-                    |> Tuple.pair rule.field)
-                |> List.sortBy (\ (_, indexes) -> List.length indexes)
-            solveWith solutions candidates = case candidates of
-                ( field, [ index ] ) :: rest -> solveWith
-                    (( field, index ) :: solutions)
-                    (List.map (\ (k, is) -> (k, List.filter (\ i -> i /= index) is)) rest)
-                _ -> solutions
-            targetIndexes = ruleCandidates
-                |> solveWith []
-                |> List.filterMap (\ ( field, index ) -> if String.startsWith "departure" field then Just index else Nothing)
-                |> Set.fromList
-        in notes.mine
-        |> List.indexedMap Tuple.pair
-        |> List.filter (\ ( index, _ ) -> Set.member index targetIndexes)
-        |> List.map Tuple.second
-        |> List.product
-        |> String.fromInt)
-    |> Maybe.withDefault ":("
+part2 string =
+    string
+        |> parseNotes
+        |> Maybe.map
+            (\notes ->
+                let
+                    validTickets =
+                        notes.nearby
+                            |> List.filter (isValidTicket notes.rules)
+
+                    ruleCandidates =
+                        notes.rules
+                            |> List.map
+                                (\rule ->
+                                    validTickets
+                                        |> List.map
+                                            (\ticket ->
+                                                ticket
+                                                    |> List.indexedMap Tuple.pair
+                                                    |> List.filterMap
+                                                        (\( index, value ) ->
+                                                            if inRule rule value then
+                                                                Just index
+
+                                                            else
+                                                                Nothing
+                                                        )
+                                                    |> Set.fromList
+                                            )
+                                        |> intersections
+                                        |> Set.toList
+                                        |> Tuple.pair rule.field
+                                )
+                            |> List.sortBy (\( _, indexes ) -> List.length indexes)
+
+                    solveWith solutions candidates =
+                        case candidates of
+                            ( field, [ index ] ) :: rest ->
+                                solveWith
+                                    (( field, index ) :: solutions)
+                                    (List.map (\( k, is ) -> ( k, List.filter (\i -> i /= index) is )) rest)
+
+                            _ ->
+                                solutions
+
+                    targetIndexes =
+                        ruleCandidates
+                            |> solveWith []
+                            |> List.filterMap
+                                (\( field, index ) ->
+                                    if String.startsWith "departure" field then
+                                        Just index
+
+                                    else
+                                        Nothing
+                                )
+                            |> Set.fromList
+                in
+                notes.mine
+                    |> List.indexedMap Tuple.pair
+                    |> List.filter (\( index, _ ) -> Set.member index targetIndexes)
+                    |> List.map Tuple.second
+                    |> List.product
+                    |> String.fromInt
+            )
+        |> Maybe.withDefault ":("
+
 
 intersections : List (Set comparable) -> Set comparable
-intersections xs = case xs of
-    [] -> Set.empty
-    x :: ys -> List.foldl Set.intersect x ys
+intersections xs =
+    case xs of
+        [] ->
+            Set.empty
+
+        x :: ys ->
+            List.foldl Set.intersect x ys
+
 
 isValidTicket : Rules -> Ticket -> Bool
-isValidTicket rules ticket = List.all (\ value -> inRules rules value) ticket
+isValidTicket rules ticket =
+    List.all (\value -> inRules rules value) ticket
+
 
 inRules : Rules -> Int -> Bool
-inRules rules value = List.any (\ rule -> inRule rule value) rules
+inRules rules value =
+    List.any (\rule -> inRule rule value) rules
+
 
 inRule : Rule -> Int -> Bool
-inRule rule value = List.any (\ range -> inRange range value) rule.ranges
+inRule rule value =
+    List.any (\range -> inRange range value) rule.ranges
+
 
 inRange : Range -> Int -> Bool
-inRange range value = range.low <= value && value <= range.high
+inRange range value =
+    range.low <= value && value <= range.high
 
 
 isValid : Int -> Range -> Bool
